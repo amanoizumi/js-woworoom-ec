130b2af3d178b9c724c1c08bafc0f0825080b645
amanoizumi <a0918233246@gmail.com>
2021-06-09 12:54:13 +0800

    update api url

--- js/config.js
+++ js/config.js
@@ -2,7 +2,9 @@
 // 分成公司帳號和客戶帳號。
 // 如果你忘記把 TOKEN 改掉，可能釀成幫客戶付錢的悲劇。
 
-const apiPath =  `https://hexschoollivejs.herokuapp.com/api/livejs/v1`;
+// const apiPath =  `https://hexschoollivejs.herokuapp.com/api/livejs/v1`;
+// 2021/06/09 新版 API 網址
+const apiPath =  `https://livejs-api.hexschool.io/api/livejs/v1`;
 const myPath = 'woworoomx';
 const config = {
   headers: {
